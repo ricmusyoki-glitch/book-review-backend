46dc0e3832925c45c67db37325a340fe8a6ec4dd
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-07-21 19:41:03 +0300

    Feature 10:Added the book average rating and review count

--- apps/books/api/serializers.py
+++ apps/books/api/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from rest_framework import serializers
 
 from apps.books.models import Book
@@ -5,6 +6,8 @@ from apps.books.models import Book
 
 class BookSerializer(serializers.ModelSerializer):
     created_by = serializers.ReadOnlyField(source="created_by.username")
+    average_rating = serializers.SerializerMethodField()
+    review_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Book
@@ -15,12 +18,29 @@ class BookSerializer(serializers.ModelSerializer):
             "description",
             "published_date",
             "created_by",
+            "average_rating",
+            "review_count",
             "created_at",
             "updated_at",
         ]
         read_only_fields = [
             "id",
             "created_by",
+            "average_rating",
+            "review_count",
             "created_at",
             "updated_at",
         ]
+
+    def get_average_rating(self, obj):
+        average = obj.reviews.aggregate(
+            Avg("rating")
+        )["rating__avg"]
+
+        if average is None:
+            return None
+
+        return round(average, 2)
+
+    def get_review_count(self, obj):
+        return obj.reviews.count()
