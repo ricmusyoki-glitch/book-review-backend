a1523fdaf1fa78517cfed1fb8dfdeb3db39c84a9
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-07-21 12:21:56 +0300

    Feature 6: Implemented JWT Authentication

--- apps/accounts/api/views.py
+++ apps/accounts/api/views.py
@@ -2,6 +2,11 @@ from rest_framework import generics, permissions
 
 from .serializers import RegisterSerializer
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from rest_framework.response import Response
+
+from rest_framework.views import APIView
 
 class RegisterView(generics.CreateAPIView):
     """
@@ -10,3 +15,32 @@ class RegisterView(generics.CreateAPIView):
 
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
+
+class LoginView(TokenObtainPairView):
+    """
+    Login user and return JWT access and refresh tokens.
+    """
+    pass
+
+
+class RefreshTokenView(TokenRefreshView):
+    """
+    Refresh the access token using a valid refresh token.
+    """
+    pass
+
+class ProfileView(APIView):
+    """
+    Return the currently authenticated user's information.
+    """
+
+    def get(self, request):
+        return Response(
+            {
+                "id": request.user.id,
+                "username": request.user.username,
+                "email": request.user.email,
+                "first_name": request.user.first_name,
+                "last_name": request.user.last_name,
+            }
+        )
